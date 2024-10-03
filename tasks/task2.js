9315c0af6faf0f75035c4e67d113112b9d8895f7
Alisa250606 <kn23-a.daieva@nubip.edu.ua>
2024-10-03 10:34:21 +0300

    YOUR COMMIT

--- tasks/task2.js
+++ tasks/task2.js
@@ -4,32 +4,44 @@ const students = [
   { name: "Jim", age: 19, score: 75 },
   { name: "Jill", age: 21, score: 65 },
   { name: "Jack", age: 23, score: 95 },
-];
+]
 
-// Ваше завдання: реалізувати наступні функції
-
-// 1. getTopScoringStudents
-// Приймає масив студентів та повертає масив імен студентів, які мають оцінку більше 80
 function getTopScoringStudents(students) {
-  // Ваш код тут
+  const topScorers = []; 
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].score > 80) {
+      topScorers.push(students[i].name); 
+    }
+  }
+  return topScorers; 
 }
 
-// 2. getAverageAge
-// Приймає масив студентів та повертає середній вік студентів
+
 function getAverageAge(students) {
-  // Ваш код тут
+  let totalAge = 0; 
+  for (let i = 0; i < students.length; i++) {
+    totalAge += students[i].age; 
+  }
+  return totalAge / students.length; 
 }
 
-// 3. addStudent
-// Приймає масив студентів та новий об'єкт студента і додає його до масиву
+
 function addStudent(students, student) {
-  // Ваш код тут
+  const newStudents = []; 
+  for (let i = 0; i < students.length; i++) {
+    newStudents.push(students[i]); 
+  }
+  newStudents.push(student); 
+  return newStudents;
 }
-
-// 4. removeStudentByName
-// Приймає масив студентів та ім'я студента, якого потрібно видалити, і повертає новий масив без цього студента
 function removeStudentByName(students, name) {
-  // Ваш код тут
+  const updatedStudents = []; 
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].name !== name) {
+      updatedStudents.push(students[i]); 
+    }
+  }
+  return updatedStudents; 
 }
 
-module.exports = { getTopScoringStudents, getAverageAge, addStudent, removeStudentByName };
+module.exports = { getTopScoringStudents, getAverageAge, addStudent, removeStudentByName};
